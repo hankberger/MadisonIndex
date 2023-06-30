7c54bb7054c4d05533258ffacc3bfa12a6608dd9
hankberger <hanksberger@gmail.com>
2023-06-29 20:21:49 -0500

    redoing market page

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -2,55 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link';
 
 export default function Market() {
-    let products: ProductProps[] = [
-        {
-            title: "Hello world",
-            price: 10
-        },
-        {
-            title: "hehe haha",
-            price: 69
-        },
-        {
-            title: "hehe hahahehe hahahehe hahahehe hahahehe hahahehe haha",
-            price: 69
-        },
-        {
-            title: "hehe haha",
-            price: 69
-        },
-        {
-            title: "hehe haha",
-            price: 69
-        },
-        {
-            title: "hehe haha",
-            price: 69
-        },
-        {
-            title: "hehe haha",
-            price: 69
-        },{
-            title: "hehe haha",
-            price: 69
-        }
-    ];
-
+   
   return (
     <>
-        <div className='flex flex-row items-center'>
-            <h1 className='text-5xl text-black m-4'>
-                Market
-            </h1>
-            <svg xmlns="http://www.w3.org/2000/svg" className="fill-black h-7 w-7" viewBox="0 0 16 16">
-                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-            </svg>
-        </div>
-        <div className='flex flex-row flex-wrap gap-7 items-center justify-around'>
-            {products.map((prod)=>{
-                return <ProductCard productDetails={prod}/>
-            })}
-        </div>
+        
     </>
   )
 }
